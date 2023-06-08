ec479c2610e3a5e2fb9840d61066b8b3b9645ff5
JunJangE <wnswkd486@gmail.com>
2023-06-08 15:50:42 +0900

    Revert "디자인 시스템 테마 정의"

--- presentation/src/main/java/com/depromeet/whatnow/ui/theme/Theme.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/theme/Theme.kt
@@ -1,52 +1,68 @@
 package com.depromeet.whatnow.ui.theme
 
 import android.app.Activity
-import androidx.compose.material3.LocalContentColor
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
 
 @Composable
-fun WhatNowTheme(
-    colors: WhatNowColors = WhatNowTheme.colors,
-    typography: WhatNowTypography = WhatNowTheme.typography,
+fun WhatnowTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val rememberedColors = remember { colors.copy() }
-
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = MaterialColors.background.toArgb()
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
         }
     }
 
-    CompositionLocalProvider(
-        LocalColors provides rememberedColors,
-        LocalTypography provides typography,
-        LocalContentColor provides White
-    ) {
-        MaterialTheme(
-            colorScheme = MaterialColors,
-            content = content
-        )
-    }
-}
-
-object WhatNowTheme {
-
-    val colors: WhatNowColors
-        @Composable
-        @ReadOnlyComposable
-        get() = LocalColors.current
-
-    val typography: WhatNowTypography
-        @Composable
-        @ReadOnlyComposable
-        get() = LocalTypography.current
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
 }
