732193ff3175692e8f90834b9e52d6f0c1071647
JunJangE <wnswkd486@gmail.com>
2023-06-09 11:46:25 +0900

    feat: HomeScreen 추가

---
+++ presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeScreen.kt
@@ -0,0 +1,24 @@
+package com.depromeet.whatnow.ui.home
+
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+
+
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
+    val context = LocalContext.current
+    val isRefresh by viewModel.isRefresh.collectAsState()
+    val launcher = rememberLauncherForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            viewModel.refresh()
+        }
+    }
+}
