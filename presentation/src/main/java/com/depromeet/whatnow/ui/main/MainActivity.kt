a75de104534a05ad44d87805d410736fff66c481
JunJangE <wnswkd486@gmail.com>
2023-06-10 21:20:39 +0900

    feat: main 폴더 추가 대응

--- presentation/src/main/java/com/depromeet/whatnow/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/main/MainActivity.kt
@@ -1,45 +1,48 @@
-package com.depromeet.whatnow.ui
+package com.depromeet.whatnow.ui.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.viewModels
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.depromeet.whatnow.ui.theme.WhatNowTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WhatNowTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen(
+                    viewModel = viewModel,
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    companion object {
+
+        fun startActivity(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     WhatNowTheme {
-        Greeting("Android")
+        MainScreen(
+            viewModel = MainViewModel(),
+        )
     }
 }
