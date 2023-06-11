6f5f1338141712b966e71f2162936af8cf9727ac
JunJangE <wnswkd486@gmail.com>
2023-06-11 16:12:35 +0900

    feat: 세팅 페이지 추가

---
+++ presentation/src/main/java/com/depromeet/whatnow/ui/setting/SettingScreen.kt
@@ -0,0 +1,22 @@
+package com.depromeet.whatnow.ui.setting
+
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+
+
+@Composable
+fun SettingScreen(viewModel: SettingViewModel = hiltViewModel()) {
+    val context = LocalContext.current
+    val isRefresh by viewModel.isRefresh.collectAsState()
+    val launcher = rememberLauncherForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            viewModel.refresh()
+        }
+    }
+}
