fa4c19d7ff0ab268c1e5bc28b5baa4de06f5d328
JunJangE <wnswkd486@gmail.com>
2023-06-11 17:27:10 +0900

    feat: HomeScreen WhatNowHomeAppBar 구성

--- presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeScreen.kt
@@ -3,13 +3,19 @@ package com.depromeet.whatnow.ui.home
 import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.depromeet.whatnow.component.WhatNowHomeAppBar
 
 
 @Composable
@@ -20,5 +26,9 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
         if (result.resultCode == Activity.RESULT_OK) {
             viewModel.refresh()
         }
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        WhatNowHomeAppBar()
     }
 }
