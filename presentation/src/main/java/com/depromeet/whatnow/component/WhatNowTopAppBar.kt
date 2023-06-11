cb780d9391ad821c47dcad0b24c4a88c2fad7b4e
JunJangE <wnswkd486@gmail.com>
2023-06-11 17:26:34 +0900

    feat: WhatNowTopAppBar 추가

---
+++ presentation/src/main/java/com/depromeet/whatnow/component/WhatNowTopAppBar.kt
@@ -0,0 +1,48 @@
+package com.depromeet.whatnow.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.depromeet.whatnow.ui.R
+
+@Composable
+fun WhatNowHomeAppBar() {
+    Surface(
+        modifier = Modifier.padding(all = 16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .width(114.dp)
+                .height(36.dp)
+        )
+    }
+}
