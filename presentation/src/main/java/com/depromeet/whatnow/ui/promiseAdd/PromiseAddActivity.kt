61c57b6ac914b72c11028aa3201f3c762c6dd7c8
Youngwoo Hyun <ATBBwsv7zbWEDkdRVPBQdYAnMELN06FCC39E>
2023-06-12 19:44:27 +0900

    feat(promise): 달력 및 시간선택 UI 작업 진행 중
    Closes #DPMAOS-23

---
+++ presentation/src/main/java/com/depromeet/whatnow/ui/promiseAdd/PromiseAddActivity.kt
@@ -0,0 +1,40 @@
+package com.depromeet.whatnow.ui.promiseAdd
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import com.depromeet.whatnow.base.BaseActivity
+import com.depromeet.whatnow.ui.theme.WhatNowTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class PromiseAddActivity : BaseActivity() {
+    private val viewModel: PromiseAddViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//        var selectedTime by mutableStateOf(LocalTime.now())
+
+        setContent {
+            WhatNowTheme {
+                PromiseScreen(
+                    viewModel = viewModel,
+                )
+            }
+        }
+    }
+
+    private fun navigateUp() = finish()
+
+    companion object {
+        fun startActivity(context: Context) {
+            val intent = Intent(context, PromiseAddActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
