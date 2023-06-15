a638a645f3457216281852e68d1e1b75556fec86
Youngwoo Hyun <ATBBwsv7zbWEDkdRVPBQdYAnMELN06FCC39E>
2023-06-15 17:51:51 +0900

    feat: 약속만들기 UI 작업 진행 중

--- presentation/src/main/java/com/depromeet/whatnow/ui/promiseAdd/PromiseAddScreen.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/promiseAdd/PromiseAddScreen.kt
@@ -1,11 +1,12 @@
 package com.depromeet.whatnow.ui.promiseAdd
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.widget.DatePicker
 import androidx.compose.foundation.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
@@ -13,13 +14,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import com.depromeet.whatnow.component.WhatNowPromiseAddBottomBar
 import com.depromeet.whatnow.ui.R
 import com.depromeet.whatnow.ui.theme.WhatNowTheme
 import java.util.*
@@ -30,9 +32,9 @@ fun PromiseScreen(viewModel: PromiseAddViewModel) {
 }
 
 @Composable
-fun Greeting(text : String, textSize: Int, textColor: Color){
+fun Greeting(resId: Int, textSize: Int, textColor: Color) {
     Text(
-        text = text,
+        text = LocalContext.current.getString(resId),
         fontSize = textSize.sp,
         color = textColor
     )
@@ -40,17 +42,18 @@ fun Greeting(text : String, textSize: Int, textColor: Color){
 
 
 @Composable
-fun setCalendar(){
+fun setCalendar() {
     Box(
         modifier = Modifier
             .padding(22.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(Color.White)
-    ){
+    ) {
         AndroidView(
             modifier = Modifier.fillMaxWidth(),
             factory = { context ->
-                val view = LayoutInflater.from(context).inflate(R.layout.item_promise_calendar, null)
+                val view =
+                    LayoutInflater.from(context).inflate(R.layout.item_promise_calendar, null)
                 val datePicker = view.findViewById<DatePicker>(R.id.datePicker1)
 
                 // 현재 날짜로 DatePicker 초기화
@@ -67,13 +70,13 @@ fun setCalendar(){
 }
 
 @Composable
-fun setClock(){
+fun setClock() {
     Box(
         modifier = Modifier
             .padding(22.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(Color.White)
-    ){
+    ) {
         AndroidView(
             modifier = Modifier.fillMaxWidth(),
             factory = { context ->
@@ -85,7 +88,7 @@ fun setClock(){
 }
 
 @Composable
-fun setMakeBox(onClick: () -> Unit,title:String , msg:String , img:Int){
+fun setMakeBox(onClick: () -> Unit, titleResId: Int, MsgResId: Int, img: Int) {
     val expanded = remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
@@ -97,9 +100,8 @@ fun setMakeBox(onClick: () -> Unit,title:String , msg:String , img:Int){
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .clickable(onClick = onClick)
         ) {
-            Greeting(title,12,WhatNowTheme.colors.gray900)
+            Greeting(titleResId, 12, WhatNowTheme.colors.gray900)
 
             Spacer(modifier = Modifier.width(8.dp))
 
@@ -120,9 +122,7 @@ fun setMakeBox(onClick: () -> Unit,title:String , msg:String , img:Int){
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp)
-                        .clickable {
-
-                        }
+                        .clickable(onClick = onClick)
                 ) {
                     Image(
                         painter = painterResource(id = img),
@@ -131,7 +131,7 @@ fun setMakeBox(onClick: () -> Unit,title:String , msg:String , img:Int){
                             .size(22.dp)
                             .padding(end = 6.dp)
                     )
-                    Greeting(msg,14,WhatNowTheme.colors.gray500)
+                    Greeting(MsgResId, 14, WhatNowTheme.colors.gray500)
                 }
             }
         }
@@ -139,6 +139,7 @@ fun setMakeBox(onClick: () -> Unit,title:String , msg:String , img:Int){
 }
 
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Preview(showBackground = false, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -146,53 +147,113 @@ fun DefaultPreview() {
     var isClockVisible by remember { mutableStateOf(false) }
 
     Surface(color = WhatNowTheme.colors.gray200) {
-        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-            setCalendar()
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1f)) {
 
-            if(!isClockVisible){
-                setMakeBox(onClick = { isClockVisible = true},"시간","몇시에 만날까요?",R.drawable.clock)
-            }else{
-                setClock()
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 22.dp, top = 22.dp)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.leftarrow),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(22.dp)
+                            .padding(end = 6.dp)
+                    )
+
+                    Greeting(R.string.promise_title, 22, WhatNowTheme.colors.whatNowBlack)
+                }
+
+
+                setCalendar()
+
+                if (!isClockVisible) {
+                    setMakeBox(onClick = { isClockVisible = true },
+                        R.string.promise_time,
+                        R.string.promise_time_msg,
+                        R.drawable.clock)
+                } else {
+                    setClock()
+                }
+
+
+//                setMakeBox(onClick = {}, "장소", "어디에사 만날까요?", R.drawable.map)
+//                Spacer(modifier = Modifier.height(12.dp))
             }
 
-            Scaffold(
-            modifier = Modifier
-                .fillMaxSize()
-                .windowInsetsPadding(WindowInsets.safeDrawing),
-            bottomBar = {
-                WhatNowPromiseAddBottomBar(
-                    modifier = Modifier
-                )
-            }
-            ){
-                it.calculateBottomPadding()
-            }
+            WhatNowPromiseAddBottomBar(modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp))
 
-//            setMakeBox(onClick={},"장소","어디에사 만날까요?",R.drawable.map)
-//
-//
-//            Spacer(modifier = Modifier.height(12.dp))
-//
-            setClock()
         }
     }
 }
 
 
 @Composable
-fun FixedBottomView() {
+fun WhatNowPromiseAddBottomBar(
+    modifier: Modifier = Modifier,
+) {
+    val bottomPanelHeight = 80.dp
+    val bottomActionButtonSize = 72.dp
+    val bottomBarItemIconSize = 20.dp
+
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+
     Box(
-        modifier = Modifier
-            .height(56.dp)
+        modifier = modifier
             .fillMaxWidth()
-            .background(Color.Gray)
     ) {
-        // 고정적인 뷰 내용
-        // ...
+        Surface(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .height(bottomPanelHeight)
+                .fillMaxWidth(),
+            color = WhatNowTheme.colors.gray900,
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 37.dp, top = 12.dp, end = 16.dp, bottom = 12.dp)
+
+                ) {
+                    Surface(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .width(74.dp)
+                            .height(56.dp)
+                            .clickable(
+                                interactionSource = MutableInteractionSource(),
+                                indication = null,
+                                onClick = {
+
+                                }
+                            ),
+                        shape = RoundedCornerShape(16.dp),
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = WhatNowTheme.colors.gray100
+                        ),
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.background(WhatNowTheme.colors.whatNowPurple)
+                        ) {
+                            Text(
+                                text = LocalContext.current.getString(R.string.promise_bottom_button_next),
+                                textAlign = TextAlign.Center,
+                                color = Color(0xEEEEEFEE)
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
-
-
-
-
