2d5090797e9b976f509cd630bb555c92bfe266aa
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-17 05:48:27 +0900

    feat: 아카이브 약속 리스트 화면 UI 구현

--- presentation/src/main/java/com/depromeet/whatnow/ui/archive/ArchiveViewModel.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/archive/ArchiveViewModel.kt
@@ -1,10 +1,13 @@
 package com.depromeet.whatnow.ui.archive
 
 import androidx.lifecycle.ViewModel
+import com.depromeet.whatnow.ui.model.DUMMY_PROMISE
+import com.depromeet.whatnow.ui.model.DUMMY_USER
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +15,26 @@ class ArchiveViewModel @Inject constructor() : ViewModel() {
 
     private val _uiState = MutableStateFlow(ArchiveState())
     val uiState: StateFlow<ArchiveState> = _uiState.asStateFlow()
+
+    init {
+        _uiState.update {
+            it.copy(
+                futurePromises = listOf(
+                    DUMMY_PROMISE(participants = List(6) { DUMMY_USER() }),
+                    DUMMY_PROMISE(participants = List(2) { DUMMY_USER() }),
+                    DUMMY_PROMISE(participants = List(1) { DUMMY_USER() })
+                ),
+                pastPromises = listOf(
+                    DUMMY_PROMISE(participants = List(6) { DUMMY_USER() }),
+                    DUMMY_PROMISE(participants = List(2) { DUMMY_USER() }),
+                    DUMMY_PROMISE(participants = List(1) { DUMMY_USER() }),
+                    DUMMY_PROMISE(participants = List(1) { DUMMY_USER() })
+                )
+            )
+        }
+    }
+
+    fun selectTab(tab: ArchiveTab) {
+        _uiState.update { it.copy(selectedTab = tab) }
+    }
 }
