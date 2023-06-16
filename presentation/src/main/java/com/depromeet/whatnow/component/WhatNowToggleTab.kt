8c4e23012546d9dac5dc48019089128d2ebf42c1
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-16 23:14:45 +0900

    feat: 공동 컴포넌트 내 ToggleTabItem 구현

--- presentation/src/main/java/com/depromeet/whatnow/component/WhatNowToggleTab.kt
+++ presentation/src/main/java/com/depromeet/whatnow/component/WhatNowToggleTab.kt
@@ -1,14 +1,20 @@
 package com.depromeet.whatnow.component
 
 import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.depromeet.whatnow.ui.theme.WhatNowTheme
 import com.depromeet.whatnow.ui.theme.White
 
 
@@ -29,8 +35,39 @@ fun WhatNowToggleTab(
         Row(
             modifier = Modifier.padding(6.dp)
         ) {
-
+            WhatNowToggleTabItem(
+                enabled = enabled,
+                textRes = enabledTextRes,
+                onClick = onEnable
+            )
+            WhatNowToggleTabItem(
+                enabled = enabled.not(),
+                textRes = disabledTextRes,
+                onClick = onDisable
+            )
         }
+    }
+}
+
+@Composable
+private fun WhatNowToggleTabItem(
+    enabled: Boolean,
+    @StringRes textRes: Int,
+    onClick: () -> Unit
+) {
+    TextButton(
+        onClick = onClick,
+        shape = RoundedCornerShape(999.dp),
+        contentPadding = PaddingValues(vertical = 8.dp, horizontal = 16.dp),
+        colors = ButtonDefaults.textButtonColors(
+            containerColor = if (enabled) WhatNowTheme.colors.whatNowPurple else White,
+            contentColor = if (enabled) White else WhatNowTheme.colors.gray500
+        )
+    ) {
+        Text(
+            text = stringResource(id = textRes),
+            style = WhatNowTheme.typography.body3
+        )
     }
 }
 
