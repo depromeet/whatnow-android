9ad2c81a54522c84781305062faa865775f30dc2
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-17 05:27:14 +0900

    feat: 임시 모델 별 더미 데이터 함수 구현

--- presentation/src/main/java/com/depromeet/whatnow/ui/model/Promise.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/model/Promise.kt
@@ -8,3 +8,10 @@ data class Promise(
     val datetime: LocalDateTime,
     val participants: List<User>
 )
+
+fun DUMMY_PROMISE(
+    title: String = "약속 이름",
+    location: String = "약속 장소",
+    datetime: LocalDateTime = LocalDateTime.now(),
+    participants: List<User> = List(5) { DUMMY_USER() }
+) = Promise(title, location, datetime, participants)
--- presentation/src/main/java/com/depromeet/whatnow/ui/model/User.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/model/User.kt
@@ -4,3 +4,8 @@ data class User(
     val name: String,
     val profileImageUrl: String
 )
+
+fun DUMMY_USER(
+    name: String = "사용자",
+    profileImageUrl: String = "https://media.licdn.com/dms/image/C5603AQHcoKPU9alW9w/profile-displayphoto-shrink_800_800/0/1644498344282?e=1692230400&v=beta&t=aK3Qau7_xpiie2xqI5hulE4H8iEbAcVZPnUXBe7-t6E"
+) = User(name, profileImageUrl)
