e357473cd762caf89a42260ac9e3e1dbe5255273
JunJangE <wnswkd486@gmail.com>
2023-06-18 19:43:26 +0900

    fix: 홈 페이지 구성 변경

--- presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeScreen.kt
@@ -3,28 +3,32 @@ package com.depromeet.whatnow.ui.home
 import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.depromeet.whatnow.component.WhatNowActivityMap
 import com.depromeet.whatnow.component.WhatNowHomeAppBar
-import com.depromeet.whatnow.component.WhatNowInactivityMap
 import com.depromeet.whatnow.component.WhatNowPromiseList
 import com.depromeet.whatnow.ui.R
-import com.depromeet.whatnow.ui.promiseactivate.PromiseActivateActivity
 import com.depromeet.whatnow.ui.theme.WhatNowTheme
 
 
@@ -43,34 +47,46 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
             .fillMaxSize()
             .background(WhatNowTheme.colors.gray50)
     ) {
-        WhatNowHomeAppBar()
+        Box() {
+            WhatNowActivityMap(modifier = Modifier)
+            WhatNowHomeAppBar(modifier = Modifier)
+        }
         Column(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
 //                .verticalScroll(rememberScrollState())
         ) {
-            Surface(
-                onClick = {
-                    PromiseActivateActivity.startActivity(context = context)
-                }
-            ) {
-                WhatNowInactivityMap(modifier = Modifier)
 
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    modifier = Modifier.padding(bottom = 17.dp),
+                    text = stringResource(R.string.upcoming_promise),
+                    style = WhatNowTheme.typography.headline3.copy(
+                        fontSize = 20.sp, color = WhatNowTheme.colors.whatNowBlack
+                    )
+                )
+
+                // 활성화된 약속이 있을 경우
+                Image(
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 8.dp, bottom= 24.dp),
+                    painter = painterResource(id = R.drawable.arrow_forward),
+                    contentDescription = null
+                )
+
+                // 활성화된 약속이 없을 경우
+//                Image(
+//                    modifier = Modifier
+//                        .align(Alignment.CenterEnd),
+//                    painter = painterResource(id = R.drawable.whatnow_home_lcon),
+//                    contentDescription = null
+//                )
             }
-            Text(
-                modifier = Modifier.padding(top = 41.dp, bottom = 17.dp),
-                text = stringResource(R.string.upcoming_promise),
-                color = Color.Black,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold
-//                            style = WhatNowTheme.typography.medium.copy(
-//                                fontSize = 16.sp,
-//                                color = WhatNowTheme.colors.gray100
-//                            )
-            )
+
             WhatNowPromiseList(
-                modifier = Modifier,
-                promises = listOf("test", "test", "test", "test")
+                modifier = Modifier, promises = listOf("test", "test", "test", "test")
             )
         }
     }
