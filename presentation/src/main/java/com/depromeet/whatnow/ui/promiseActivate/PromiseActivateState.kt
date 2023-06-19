59b855d3a877c5d142d2b6a23f03b89a589b77d8
junjange <wnswkd486@naver.com>
2023-06-19 17:32:55 +0900

    feat: PromiseActivateState 추가

--- presentation/src/main/java/com/depromeet/whatnow/ui/promiseActivate/PromiseActivateState.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/promiseActivate/PromiseActivateState.kt
@@ -1,6 +1,12 @@
 package com.depromeet.whatnow.ui.promiseActivate
 
+import com.depromeet.whatnow.ui.model.Promise
+
 data class PromiseActivateState(
     val showTooltip: Boolean = false,
     val isAudioPlaying: Boolean = false,
+    val selectedTab: PromiseActivateTab = PromiseActivateTab.Total,
+    val totalProfile: List<Promise> = emptyList(),
+    val myProfile: List<Promise> = emptyList(),
+    val friendProfile: List<Promise> = emptyList()
 )
