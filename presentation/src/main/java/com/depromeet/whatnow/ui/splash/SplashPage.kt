32504f21c27c4720b2880dd4d93e109b97b1d80b
Youngwoo Hyun <ATBBwsv7zbWEDkdRVPBQdYAnMELN06FCC39E>
2023-06-20 18:24:43 +0900

    feat: Splash 액티비티 및 스크린 추가 (아직 작업 전 - UI 다 안나옴)

---
+++ presentation/src/main/java/com/depromeet/whatnow/ui/splash/SplashPage.kt
@@ -0,0 +1,9 @@
+package com.depromeet.whatnow.ui.splash
+
+import com.depromeet.whatnow.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashPage : BaseActivity() {
+
+}
