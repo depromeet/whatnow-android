27af74191414c4d6fc6cce58598428560c98d89e
yjyoon-dev <yjyoon.dev@gmail.com>
2023-06-25 04:25:14 +0900

    feat: user 임시 모델에서 도착 상태를 랜덤으로 지정하도록 수정

--- presentation/src/main/java/com/depromeet/whatnow/ui/model/User.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/model/User.kt
@@ -12,6 +12,6 @@ data class User(
 fun DUMMY_USER(
     name: String = "사용자",
     profileImageUrl: String = "https://media.licdn.com/dms/image/C5603AQHcoKPU9alW9w/profile-displayphoto-shrink_800_800/0/1644498344282?e=1692230400&v=beta&t=aK3Qau7_xpiie2xqI5hulE4H8iEbAcVZPnUXBe7-t6E",
-    arrivalState: ArrivalState = ArrivalState.Arrive,
+    arrivalState: ArrivalState = ArrivalState.values().toList().shuffled().first(),
     emojis: Map<EmojiInteraction, Int> = DUMMY_EMOJI()
 ) = User(name, profileImageUrl, arrivalState, emojis)
