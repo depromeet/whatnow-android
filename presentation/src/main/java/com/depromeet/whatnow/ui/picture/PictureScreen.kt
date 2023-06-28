4b65444fb5bc8c6923d51618df8e5ab1a0d405bf
JunJangE <wnswkd486@gmail.com>
2023-06-28 15:10:26 +0900

    feat: PictureScreen

---
+++ presentation/src/main/java/com/depromeet/whatnow/ui/picture/PictureScreen.kt
@@ -0,0 +1,26 @@
+package com.depromeet.whatnow.ui.picture
+
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.depromeet.whatnow.component.WhatNowBottomSheetScaffold
+import com.depromeet.whatnow.ui.promiseActivate.myStatus.MyStatusScreen
+
+
+@Composable
+fun PictureScreen(viewModel: PictureViewModel = hiltViewModel()) {
+    val context = LocalContext.current
+    val isRefresh by viewModel.isRefresh.collectAsState()
+    val launcher = rememberLauncherForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            viewModel.refresh()
+        }
+    }
+}
