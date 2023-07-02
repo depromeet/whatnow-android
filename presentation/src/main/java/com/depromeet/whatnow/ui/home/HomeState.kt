ed5cdb5aa776f055a19d1e22b273d802d7575bf2
junjange <wnswkd486@naver.com>
2023-07-02 18:23:26 +0900

    fix: 다가오는 약속에 따라서 약속 생성 버튼 변경

--- presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeState.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/home/HomeState.kt
@@ -3,5 +3,5 @@ package com.depromeet.whatnow.ui.home
 data class HomeState(
     val showTooltip: Boolean = false,
     val isAudioPlaying: Boolean = false,
-    val currentStatus: HomeActivateStatus = HomeActivateStatus.InActivity,
+    val currentStatus: HomeActivateStatus = HomeActivateStatus.Activity,
 )
