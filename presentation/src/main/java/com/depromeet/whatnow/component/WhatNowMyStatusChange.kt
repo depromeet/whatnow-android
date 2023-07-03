e77fdab0072792e3b62e34ec116731310be6b4ee
JunJangE <wnswkd486@gmail.com>
2023-07-03 18:00:33 +0900

    fix: 내 상태 변경 패딩 값 변경

--- presentation/src/main/java/com/depromeet/whatnow/component/WhatNowMyStatusChange.kt
+++ presentation/src/main/java/com/depromeet/whatnow/component/WhatNowMyStatusChange.kt
@@ -1,17 +1,14 @@
 package com.depromeet.whatnow.component
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -19,9 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.depromeet.whatnow.ui.R
 import com.depromeet.whatnow.ui.model.DUMMY_PROMISE
 import com.depromeet.whatnow.ui.theme.WhatNowTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -42,15 +40,13 @@ fun WhatNowMyStatusChange(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             HorizontalPager(
-                modifier = Modifier,
                 state = pagerState,
                 verticalAlignment = Alignment.CenterVertically,
                 pageCount = 4,
-                contentPadding = PaddingValues(horizontal = 24.dp),
-                pageSpacing = 8.dp
+                contentPadding = PaddingValues(horizontal = 116.34.dp),
             ) {
                 Box(
-                    modifier = Modifier
+                    contentAlignment = Alignment.Center
                 ) {
                     AsyncImage(
                         model = DUMMY_PROMISE().participants[0].profileImageUrl,
@@ -63,6 +59,12 @@ fun WhatNowMyStatusChange(
                                 color = WhatNowTheme.colors.gray50,
                                 shape = RoundedCornerShape(24.dp)
                             )
+                    )
+
+                    Image(
+                        modifier = Modifier.size(200.dp),
+                        painter = painterResource(R.drawable.status_change_washing_img),
+                        contentDescription = null
                     )
                 }
             }
