93982eac79aac2aa24e351571df79098a828dc9c
Youngwoo Hyun <ATBBwsv7zbWEDkdRVPBQdYAnMELN06FCC39E>
2023-07-04 17:01:23 +0900

    feat: Login API 작업 진행 중

---
+++ domain/src/main/java/com/depromeet/whatnow/domain/usecase/LoginUseCase.kt
@@ -0,0 +1,14 @@
+package com.depromeet.whatnow.domain.usecase
+
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+//    private val authRepository: AuthRepository,
+//    private val usersRepository: UsersRepository,
+) {
+//    suspend operator fun invoke(
+//        providerToken: ProviderToken,
+//    ): Result<LoginResult> = runCatching {
+//
+//    }
+}
