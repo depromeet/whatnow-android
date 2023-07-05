18231877bf5a61e0528a0649ac9662e339cee159
JunJangE <wnswkd486@gmail.com>
2023-07-05 17:45:18 +0900

    feat: PromisesLocationResponse

---
+++ data/src/main/java/com/depromeet/whatnow/data/model/response/PromisesLocationResponse.kt
@@ -0,0 +1,9 @@
+package com.depromeet.whatnow.data.model.response
+
+import com.depromeet.whatnow.domain.model.CoordinateVo
+import com.google.gson.annotations.SerializedName
+
+data class PromisesLocationResponse(
+    @SerializedName("coordinate") val coordinate: CoordinateVo,
+    @SerializedName("address") val address: String
+)
