739bef9bc3327a623e593c7cfc5504f38fde8e69
JunJangE <wnswkd486@gmail.com>
2023-07-06 13:02:31 +0900

    feat: 월단위 약속 조회 api 연결

--- domain/src/main/java/com/depromeet/whatnow/domain/repo/PromisesRepository.kt
+++ domain/src/main/java/com/depromeet/whatnow/domain/repo/PromisesRepository.kt
@@ -3,7 +3,11 @@ package com.depromeet.whatnow.domain.repo
 
 import com.depromeet.whatnow.domain.model.Location
 import com.depromeet.whatnow.domain.model.NcpMapInfo
+import com.depromeet.whatnow.domain.model.PromisesMonthlyUserList
 
 interface PromisesRepository {
-    suspend fun getLocation(request : Location): Result<NcpMapInfo>
+    suspend fun getLocation(request: Location): Result<NcpMapInfo>
+
+    suspend fun getPromisesMonthlyUsers(year_month: String): Result<PromisesMonthlyUserList>
+
 }
