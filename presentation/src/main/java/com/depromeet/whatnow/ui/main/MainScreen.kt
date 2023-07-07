d4fee105b6aaf4f778e453174ba04d886e0621ba
JunJangE <wnswkd486@gmail.com>
2023-07-07 21:13:27 +0900

    feat: 다가오는 약속에 따라 버튼 텍스트 변경

--- presentation/src/main/java/com/depromeet/whatnow/ui/main/MainScreen.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/main/MainScreen.kt
@@ -1,43 +1,29 @@
 package com.depromeet.whatnow.ui.main
 
 import android.annotation.SuppressLint
-import android.util.Log
+import android.os.Build
 import androidx.activity.compose.BackHandler
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.depromeet.whatnow.component.NoRippleInteractionSource
 import com.depromeet.whatnow.component.WhatNowBottomBar
-import com.depromeet.whatnow.component.WhatNowBottomSheetContent
 import com.depromeet.whatnow.component.bottomPanelHeight
-import com.depromeet.whatnow.ui.R
-import com.depromeet.whatnow.ui.history.HistoryScreen
 import com.depromeet.whatnow.ui.home.HomeScreen
-import com.depromeet.whatnow.ui.promiseAdd.PromiseAddActivity
-import com.depromeet.whatnow.ui.theme.WhatNowTheme
 import kotlinx.coroutines.launch
 
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(
     ExperimentalMaterial3Api::class,
@@ -54,6 +40,7 @@ fun MainScreen(
 
     val navController = rememberNavController()
     val navigator = rememberNavigator(navController = navController)
+    val uiState by viewModel.uiState.collectAsState()
 
     val coroutineScope = rememberCoroutineScope()
     val modalBottomSheetState =
@@ -67,7 +54,7 @@ fun MainScreen(
         bottomBar = {
             WhatNowBottomBar(
                 onNavigate = { navigator.navigateTo(it) },
-                isPromise = true
+                isPromise = uiState.promisesUsersStatus.isNotEmpty()
             )
         },
     ) {
