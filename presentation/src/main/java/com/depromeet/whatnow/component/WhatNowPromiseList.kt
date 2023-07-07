311263d419b748a201768ab2461b16c5d0376231
JunJangE <wnswkd486@gmail.com>
2023-07-07 22:16:20 +0900

    fix: 다가오는 약속 구현

--- presentation/src/main/java/com/depromeet/whatnow/component/WhatNowPromiseList.kt
+++ presentation/src/main/java/com/depromeet/whatnow/component/WhatNowPromiseList.kt
@@ -2,40 +2,28 @@ package com.depromeet.whatnow.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.depromeet.whatnow.ui.theme.WhatNowTheme
+import com.depromeet.whatnow.domain.model.GetPromisesUsersStatus
 
 @Composable
 fun WhatNowPromiseList(
     modifier: Modifier = Modifier,
-    promises: List<String>
+    promisesUsersStatus: List<GetPromisesUsersStatus>
 ) {
     LazyColumn(
-        modifier = modifier.height(((promises.size * 80) + 80).dp),
+        modifier = modifier.height(((promisesUsersStatus.size * 80) + 80).dp),
         userScrollEnabled = false
     ) {
-        items(promises.size) {
+        items(promisesUsersStatus.size) {
             Column {
                 WhatNowPromise(
-                    modifier = modifier
+                    modifier = modifier, promisesUsersStatusItem = promisesUsersStatus[it]
                 )
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun WhatNowPromiseListPreview() {
-    WhatNowTheme {
-        WhatNowPromiseList(Modifier, listOf("1", "2", "3"))
     }
 }
 
