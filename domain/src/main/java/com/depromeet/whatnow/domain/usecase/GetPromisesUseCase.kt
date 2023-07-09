a67780ca69eaa84f90397c44e7da26797b84a33b
JunJangE <wnswkd486@gmail.com>
2023-07-09 21:29:33 +0900

    fix: GetPromisesUseCase return

--- domain/src/main/java/com/depromeet/whatnow/domain/usecase/GetPromisesUseCase.kt
+++ domain/src/main/java/com/depromeet/whatnow/domain/usecase/GetPromisesUseCase.kt
@@ -1,11 +1,12 @@
 package com.depromeet.whatnow.domain.usecase
 
+import com.depromeet.whatnow.domain.model.GetPromises
 import com.depromeet.whatnow.domain.repo.PromisesRepository
 import javax.inject.Inject
 
 class GetPromisesUseCase @Inject constructor(
     private val promisesRepository: PromisesRepository,
 ) {
-    suspend operator fun invoke(promise_id: Int) =
-        runCatching { promisesRepository.getPromises(promise_id = promise_id) }
+    suspend operator fun invoke(promise_id: Int): Result<GetPromises> =
+        promisesRepository.getPromises(promise_id = promise_id)
 }
