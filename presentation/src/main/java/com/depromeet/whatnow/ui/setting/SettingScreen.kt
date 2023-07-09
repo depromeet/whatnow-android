ade2883a8d3717a17cbbf070d11572973bdecbf1
yjyoon-dev <yjyoon.dev@gmail.com>
2023-07-09 16:06:25 +0900

    chore: 회원 탈퇴 화면 관련 리소스 추가

--- presentation/src/main/java/com/depromeet/whatnow/ui/setting/SettingScreen.kt
+++ presentation/src/main/java/com/depromeet/whatnow/ui/setting/SettingScreen.kt
@@ -270,7 +270,7 @@ fun SettingScreen(
                         onClick = {}
                     )
                     WhatNowButtonBar(
-                        textRes = R.string.delete_user,
+                        textRes = R.string.withdraw_whatnow,
                         onClick = {}
                     )
                 }
