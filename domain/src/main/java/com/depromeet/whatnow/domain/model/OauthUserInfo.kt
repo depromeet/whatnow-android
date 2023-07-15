91d2855b94662b0d2b270d83988015c4ac9a6954
YW <ieeh0122@naver.com>
2023-07-15 16:51:05 +0900

    fix: Oauth User 정보 이메일 부분 제거

--- domain/src/main/java/com/depromeet/whatnow/domain/model/OauthUserInfo.kt
+++ domain/src/main/java/com/depromeet/whatnow/domain/model/OauthUserInfo.kt
@@ -1,7 +1,6 @@
 package com.depromeet.whatnow.domain.model
 
 data class OauthUserInfo(
-    val email: String,
     val profileImage: String,
     val isDefaultImage: Boolean,
     val nickname: String,
