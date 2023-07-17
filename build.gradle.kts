5f88081be73bfb3e4d16b203cfb082f36629efd4
Youngwoo Hyun <ATBBwsv7zbWEDkdRVPBQdYAnMELN06FCC39E>
2023-07-17 16:22:54 +0900

    feat: 약속 초대 링크보내기 구현 중

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 buildscript {
     dependencies {
         classpath(libs.kotlin.gradle.plugin)
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
 
 plugins {
-    alias(libs.plugins.com.android.application) apply(false)
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply(false)
-    alias(libs.plugins.com.google.hilt.android) apply(false)
-    alias(libs.plugins.secrets.gradle.plugin) apply(false)
+    alias(libs.plugins.com.android.application) apply (false)
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply (false)
+    alias(libs.plugins.com.google.hilt.android) apply (false)
+    alias(libs.plugins.secrets.gradle.plugin) apply (false)
 }
