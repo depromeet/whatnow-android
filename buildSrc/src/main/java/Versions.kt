aee203914b2e0fdb52428007d0337c15a8f46a30
YW <ieeh0122@naver.com>
2023-07-21 01:15:48 +0900

    fix: 버전 업

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val COMPILE_SDK = 33
     const val MIN_SDK = 24
 
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.0.0"
+    const val VERSION_CODE = 5
+    const val VERSION_NAME = "1.0.2"
 }
